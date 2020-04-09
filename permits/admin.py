1b27f43619a3b0ef7754e8cff27bac3f9a949499
Todd Schuette <wgmcco@gmail.com>
2020-04-09 10:15:40 -0700

    Updated models refine

--- permits/admin.py
+++ permits/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 
-from .models import ContactProfile, CustomerProfile, InsuranceProfile, VehicleProfile, AgencyProfile, PermitProfile
+from .models import ContactProfile, SubProfile, InsuranceProfile, VehicleProfile, AgencyProfile, PermitProfile
 
 admin.site.register(ContactProfile)
-admin.site.register(CustomerProfile)
+admin.site.register(SubProfile)
 admin.site.register(InsuranceProfile)
 admin.site.register(VehicleProfile)
 admin.site.register(AgencyProfile)
